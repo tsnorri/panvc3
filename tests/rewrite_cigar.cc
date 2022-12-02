6b65051a1fb2b73870d98d61bc262cdbdc9475bc
Tuukka Norri <tsnorri@iki.fi>
2022-12-02 18:11:56 +0200

    Remove unneeded header

--- tests/rewrite_cigar.cc
+++ tests/rewrite_cigar.cc
@@ -4,7 +4,6 @@
  */
 
 #include <catch2/catch.hpp>
-#include <libbio/assert.hh>
 #include <libbio/file_handling.hh>
 #include <libbio/generic_parser.hh>
 #include <libbio/generic_parser/cigar_field.hh>
