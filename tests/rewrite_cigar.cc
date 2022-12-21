bf54e2f1b4cb25c04c2b4417655d48eaacc85005
Tuukka Norri <tsnorri@iki.fi>
2022-12-21 15:29:39 +0200

    Use new libbio API for parsing test input

--- tests/rewrite_cigar.cc
+++ tests/rewrite_cigar.cc
@@ -7,7 +7,7 @@
 #include <libbio/file_handling.hh>
 #include <libbio/generic_parser.hh>
 #include <libbio/generic_parser/cigar_field.hh>
-#include <libbio/utility/tuple_slice.hh>
+#include <libbio/tuple/slice.hh>
 #include <panvc3/rewrite_cigar.hh>
 #include "test_additions.hh"
 
@@ -19,25 +19,41 @@ namespace tests	= panvc3::tests;
 
 namespace {
 	
-	struct input_fixture
+	struct input_fixture_
 	{
 		std::string					src;
 		std::string 				dst;
 		std::string					dst_without_gaps;
+		std::string					given;
 		panvc3::sequence_entry_pair	seq_entry_pair;
 		
-		input_fixture() = default;
+		input_fixture_() = default;
 		
-		template <typename t_src, typename t_dst>
-		input_fixture(
+		template <typename t_src, typename t_dst, typename t_given>
+		input_fixture_(
 			t_src &&src_,
-			t_dst &&dst_
+			t_dst &&dst_,
+			t_given &&given_
 		):
 			src(std::forward <t_src>(src_)),
 			dst(std::forward <t_dst>(dst_)),
-			dst_without_gaps(tests::copy_without_gaps(dst))
+			dst_without_gaps(tests::copy_without_gaps <std::string>(dst)),
+			given(std::forward <t_given>(given_))
 		{
 			panvc3::make_sequence_entry_pair(src, dst, seq_entry_pair);
+		}
+	};
+	
+	struct input_fixture : public input_fixture_
+	{
+		using input_fixture_::input_fixture_;
+		
+		input_fixture &operator=(input_fixture &&other) &
+		{
+			input_fixture_::operator=(std::move(other));
+			seq_entry_pair.first.fix_rank_select_pointers();
+			seq_entry_pair.second.fix_rank_select_pointers();
+			return *this;
 		}
 	};
 	
@@ -61,6 +77,32 @@ namespace {
 		lbp::traits::delimited <lbp::delimiter <'\t'>, lbp::delimiter <'\n'>>,
 		t_args...
 	>;
+	
+	
+	struct sequence_pair_tag : public lbp::empty_tag {};
+	struct query_tag : public lbp::empty_tag {};
+	
+	struct conditional_field
+	{
+		template <typename t_caller>
+		bool parse(t_caller &&caller) const
+		{
+			auto &range(caller.range());
+			switch (*range.it)
+			{
+				case 'S':
+					caller.read_delimiter();
+					return caller.continue_parsing(sequence_pair_tag{});
+					
+				case 'Q':
+					caller.read_delimiter();
+					return caller.continue_parsing(query_tag{});
+					
+				default:
+					throw lbp::parse_error_tpl(lbp::errors::unexpected_character(*range.it));
+			}
+		}
+	};
 }
 
 
@@ -71,119 +113,79 @@ SCENARIO("rewrite_cigar() can handle predefined inputs", "[rewrite_cigar]")
 	lb::file_istream stream;
 	lb::open_file_for_reading("rewrite_cigar_inputs.tsv", stream);
 	
-	typedef lbp::parser <
-		lbp::traits::delimited <lbp::delimiter <'\t'>>,
-		lbp::fields::character
-	> record_type_column_parser_type;
+	typedef lbp::traits::delimited <lbp::delimiter <'\t'>>							parser_traits;
+	typedef lbp::traits::delimited <lbp::delimiter <'\t'>, lbp::delimiter <'\n'>>	conditional_parser_traits;
 	
-	typedef parser <lbp::fields::text <>, lbp::fields::text <>, lbp::fields::text <>>	sequence_pair_parser_type;
-	typedef parser <
-		lbp::fields::text <>,
-		lbp::fields::numeric <std::uint16_t>,
-		lbp::fields::numeric <std::uint16_t>,
-		lbp::fields::cigar_field,
-		lbp::fields::cigar_field,
-		lbp::fields::text <>, 
-		lbp::fields::text <>
-	> query_parser_type;
+	typedef lbp::parser <
+		parser_traits,
+		lbp::fields::make_conditional <
+			conditional_field,
+			conditional_parser_traits,
+			lbp::fields::option <
+				sequence_pair_tag,
+				lbp::fields::text <>,
+				lbp::fields::text <>,
+				lbp::fields::text <>
+			>,
+			lbp::fields::option <
+				query_tag,
+				lbp::fields::text <>,
+				lbp::fields::numeric <std::uint16_t>,
+				lbp::fields::numeric <std::uint16_t>,
+				lbp::fields::cigar,
+				lbp::fields::cigar,
+				lbp::fields::text <>, 
+				lbp::fields::text <>
+			>
+		>
+	> parser_type;
 	
 	std::istreambuf_iterator it(stream);
 	std::istreambuf_iterator <char> const sentinel;
 	auto range(lbp::make_range(it, sentinel));
 	
-	record_type_column_parser_type record_type_parser;
-	sequence_pair_parser_type sequence_pair_parser;
-	query_parser_type query_parser;
+	parser_type parser;
+	parser_type::record_type rec;
+	parser_type::buffer_type buffer;
 	
-	record_type_column_parser_type::record_type record_type_tuple;
-	sequence_pair_parser_type::record_type sequence_pair_tuple;
-	query_parser_type::record_type query_tuple;
-	
-	// Initial record.
-	{
-		auto const status(record_type_parser.parse(range, record_type_tuple));
-		CHECK(status);
-		CHECK(std::get <0>(record_type_tuple) == 'S');
-	}
-	
-	while (true)
-	{
-		// Parse the sequence pair record.
-		{
-			auto const status(sequence_pair_parser.parse(range, sequence_pair_tuple));
-			REQUIRE(status);
-		}
-		
-		// Set up the sequence fixture.
-		input_fixture fixture(std::get <1>(sequence_pair_tuple), std::get <2>(sequence_pair_tuple));
-		
-		while (true)
-		{
-			// Parse the inputs for the fixture.
-			{
-				auto const status(record_type_parser.parse(range, record_type_tuple));
+	input_fixture fixture;
+	parser.parse_all(range, rec, buffer, tests::overloaded{
+		[&fixture]<typename t_tuple>(t_tuple const &tup) requires tests::type_matches_tuple_element_v <sequence_pair_tag, t_tuple> {
+			auto const &[tag, inputs_given, src_seq, dst_seq] = tup;
 			
-				// Stop if we got an EOF.
-				if (!status)
-					return;
-			}
+			// Update the fixture.
+			fixture = input_fixture(src_seq, dst_seq, inputs_given);
+		},
+		[&fixture]<typename t_tuple>(t_tuple const &tup) requires tests::type_matches_tuple_element_v <query_tag, t_tuple> {
+			auto const &[tag, query, src_pos, expected_dst_pos, cigar, expected_cigar, section_name, query_given] = tup;
 			
-			auto const op(std::get <0>(record_type_tuple));
-			switch (op)
+			SECTION(section_name)
 			{
-				case 'S':
-					goto continue_outer_loop;
-				
-				case 'Q':
+				GIVEN(fixture.given)
 				{
+					AND_GIVEN(query_given)
 					{
-						auto const status(query_parser.parse(range, query_tuple));
-						REQUIRE(status);
-					}
-				
-					SECTION(std::get <5>(query_tuple))
-					{
-						GIVEN(std::get <0>(sequence_pair_tuple))
+						auto const &seq_entry_pair(fixture.seq_entry_pair);
+						auto const &dst(fixture.dst_without_gaps);
+						
+						WHEN("the CIGAR string is rewritten")
 						{
-							AND_GIVEN(std::get <6>(query_tuple))
+							panvc3::cigar_buffer buffer;
+							auto const dst_pos(rewrite_cigar(src_pos, cigar, seq_entry_pair, query, dst, buffer));
+							
+							THEN("the new position and the rewritten CIGAR match the expected")
 							{
-								std::apply(
-									[&fixture]
-									(auto const &query, auto const src_pos, auto const expected_dst_pos, auto const &cigar, auto const &expected_cigar){
-										auto const &seq_entry_pair(fixture.seq_entry_pair);
-										auto const &dst(fixture.dst_without_gaps);
-										
-										WHEN("the CIGAR string is rewritten")
-										{
-											panvc3::cigar_buffer buffer;
-											auto const dst_pos(rewrite_cigar(src_pos, cigar, seq_entry_pair, query, dst, buffer));
-											
-											THEN("the new position and the rewritten CIGAR match the expected")
-											{
-												INFO("expected_dst_pos: " << expected_dst_pos);
-												INFO("actual:           " << dst_pos);
-												INFO("expected_cigar:   " << tests::to_readable(expected_cigar));
-												INFO("actual:           " << tests::to_readable(buffer.operations()));
-												CHECK(expected_dst_pos == dst_pos);
-												CHECK(panvc3::cigar_eq <true>(expected_cigar, buffer.operations()));
-											}
-										}
-									},
-									lb::tuple_slice <0, 5, true>(query_tuple)
-								);
+								INFO("expected_dst_pos: " << expected_dst_pos);
+								INFO("actual:           " << dst_pos);
+								INFO("expected_cigar:   " << tests::to_readable(expected_cigar));
+								INFO("actual:           " << tests::to_readable(buffer.operations()));
+								CHECK(expected_dst_pos == dst_pos);
+								CHECK(panvc3::cigar_eq <true>(expected_cigar, buffer.operations()));
 							}
 						}
 					}
-					
-					break;
 				}
-			
-				default:
-					FAIL("Unexpected input");
 			}
 		}
-		
-	continue_outer_loop:
-		;
-	}
+	});
 }
