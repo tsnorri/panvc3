df6a796160a5693c07b2e2d5e44b63c0f5d706ac
Tuukka Norri <tsnorri@iki.fi>
2023-01-13 11:46:12 +0200

    Fix tabs

--- make/local.linux-llvm.mk
+++ make/local.linux-llvm.mk
@@ -3,15 +3,15 @@
 LLVM_ROOT				= /usr/lib/llvm-15
 CLANG_INCLUDE_DIR		= $(LLVM_ROOT)/lib/clang/15/include
 
-CC							= clang-15
-CXX							= clang++-15
-LIBDISPATCH_CFLAGS			= -U__STDC_HOSTED__ -isystem $(CLANG_INCLUDE_DIR) 
-LIBDISPATCH_CXXFLAGS		= -U__STDC_HOSTED__ -isystem $(CLANG_INCLUDE_DIR) -stdlib=libc++
+CC						= clang-15
+CXX						= clang++-15
+LIBDISPATCH_CFLAGS		= -U__STDC_HOSTED__ -isystem $(CLANG_INCLUDE_DIR) 
+LIBDISPATCH_CXXFLAGS	= -U__STDC_HOSTED__ -isystem $(CLANG_INCLUDE_DIR) -stdlib=libc++
 
 CPPFLAGS				= -DBOOST_NO_CXX98_FUNCTION_BASE
 CFLAGS					= -fblocks -U__STDC_HOSTED__ -isystem $(CLANG_INCLUDE_DIR)
 CXXFLAGS				= -fblocks -U__STDC_HOSTED__ -isystem $(CLANG_INCLUDE_DIR) -stdlib=libc++
-WARNING_FLAGS_		= -Wno-deprecated-builtins
+WARNING_FLAGS_			= -Wno-deprecated-builtins
 LDFLAGS					= -static -static-libgcc -stdlib=libc++ -lc++ -lpthread -lbsd -lz -ldl
 
 BOOST_ROOT				= /home/tnorri/local/boost-1.80.0-clang-15
