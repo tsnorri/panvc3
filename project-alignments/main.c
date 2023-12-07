23c309ce18d665d5b0998641cbb4d708a81a9b8b
Tuukka Norri <tsnorri@iki.fi>
2023-12-07 15:41:45 +0200

    Add note

--- project-alignments/main.c
+++ project-alignments/main.c
@@ -5,11 +5,7 @@
 
 #include <stdlib.h> // EXIT_SUCCESS
 
-// The compiler (as of GCC 12) seems to optimise process() in project_alignments.cc
-// in some way that interferes with the call to dispatch_main(). This results in
-// "terminate called without an active exception" on Linux but not on macOS at least
-// with some inputs. The solution is to have process() in another compilation unit;
-// for extra safety main() is now in pure C.
+// FIXME: This file is no longer needed; we can use the C++ compiler to compile main now that GCD is not used.
 
 extern void panvc3_project_alignments(int argc, char **argv);
 
