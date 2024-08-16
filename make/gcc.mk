a16333fa91d849d5ac608e8ac0098d32acc6b6cb
Tuukka Norri <tsnorri@iki.fi>
2024-08-16 13:36:56 +0300

    Set CC, CXX only if not already set

--- make/gcc.mk
+++ make/gcc.mk
@@ -7,8 +7,8 @@ endif
 GCC_ROOT			?= /usr
 GCC					?= $(GCC_ROOT)/bin/gcc$(GCC_SUFFIX)
 GXX					?= $(GCC_ROOT)/bin/g++$(GCC_SUFFIX)
-CC					= $(GCC)
-CXX					= $(GXX)
+CC					?= $(GCC)
+CXX					?= $(GXX)
 
 BOOST_ROOT_GCC		?= /usr
 BOOST_ROOT			?= $(BOOST_ROOT_GCC)
