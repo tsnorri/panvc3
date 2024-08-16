3f92b2e389affdb7f3e040fad7543cb781000b08
Tuukka Norri <tsnorri@iki.fi>
2024-08-16 13:45:03 +0300

    Fix variable assignments

--- make/gcc.mk
+++ make/gcc.mk
@@ -7,8 +7,14 @@ endif
 GCC_ROOT			?= /usr
 GCC					?= $(GCC_ROOT)/bin/gcc$(GCC_SUFFIX)
 GXX					?= $(GCC_ROOT)/bin/g++$(GCC_SUFFIX)
-CC					?= $(GCC)
-CXX					?= $(GXX)
+
+ifeq "$(origin CC)" "default"
+CC					= $(GCC)
+endif
+
+ifeq "$(origin CXX)" "default"
+CXX					= $(GXX)
+endif
 
 BOOST_ROOT_GCC		?= /usr
 BOOST_ROOT			?= $(BOOST_ROOT_GCC)
