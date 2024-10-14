d573208d6597390c660e458fe197127d8a64125e
Tuukka Norri <tsnorri@iki.fi>
2024-10-14 12:11:24 +0300

    Make alignment_input adapter class for SAM and in-order BAM reader

--- include/panvc3/alignment_input.hh
+++ include/panvc3/alignment_input.hh
@@ -6,31 +6,129 @@
 #ifndef PANVC3_ALIGNMENT_INPUT_HH
 #define PANVC3_ALIGNMENT_INPUT_HH
 
+#include <libbio/bam/in_order_streaming_reader.hh>
+#include <libbio/dispatch.hh>
 #include <libbio/file_handle.hh>
 #include <libbio/sam.hh>
 #include <utility>					// std::forward
 
-
 namespace panvc3 {
+	
+	struct alignment_input_delegate
+	{
+		virtual ~alignment_input_delegate() {}
+		virtual void handle_header(libbio::sam::header &header) = 0;
+		virtual void handle_alignment(libbio::sam::record &rec) = 0;
+	};
+}
+
+
+namespace panvc3::detail {
 	
 	struct alignment_input
 	{
-		libbio::sam::file_handle_input_range_	input_range;
-		libbio::sam::header						header;
-		libbio::sam::reader						reader;
+		virtual ~alignment_input() {}
+		virtual void run() = 0;
+	};
+	
+	
+	class sam_alignment_input final : public alignment_input
+	{
+	private:
+		libbio::sam::reader						m_reader;
+		libbio::sam::header						m_header;
+		libbio::sam::file_handle_input_range_	m_input_range;
+		libbio::dispatch::serial_queue_base		*m_output_queue{};
+		libbio::dispatch::group					*m_group{};
+		alignment_input_delegate				*m_delegate{};
 		
-		explicit alignment_input(libbio::file_handle &&fh):
-			input_range(std::move(fh))
+	public:
+		sam_alignment_input(
+			libbio::file_handle &&fh,
+			libbio::dispatch::serial_queue_base &output_queue,
+			libbio::dispatch::group &group,
+			alignment_input_delegate &delegate
+		):
+			m_input_range(std::move(fh)),
+			m_output_queue(&output_queue),
+			m_group(&group),
+			m_delegate(&delegate)
 		{
-			input_range.prepare();
+			m_input_range.prepare();
 		}
 		
-		static alignment_input open_path_or_stdin(char const *path);
+		void run() override;
+	};
+	
+	
+	class bam_in_order_alignment_input final :	public alignment_input,
+												public libbio::bam::in_order_streaming_reader_delegate
+	{
+	private:
+		libbio::file_handle						m_input_handle;
+		libbio::dispatch::queue					*m_reading_queue{};
+		libbio::dispatch::group					*m_group{};
+		alignment_input_delegate				*m_delegate{};
+		libbio::bam::in_order_streaming_reader	m_bam_reader;
+		libbio::bgzf::streaming_reader			m_bgzf_reader;
+		libbio::sam::header						m_header;
 		
-		void read_header() { reader.read_header(header, input_range); }
+	public:
+		bam_in_order_alignment_input(
+			libbio::file_handle &&fh,
+			libbio::dispatch::queue &reading_queue,
+			libbio::dispatch::serial_queue_base &output_queue,
+			libbio::dispatch::group &group,
+			alignment_input_delegate &delegate
+		):
+			m_input_handle(std::move(fh)),
+			m_reading_queue(&reading_queue),
+			m_group(&group),
+			m_delegate(&delegate),
+			m_bam_reader(output_queue, *m_group, *this),
+			m_bgzf_reader(m_input_handle, *m_group, m_bam_reader)
+		{
+		}
 		
-		template <typename t_cb>
-		void read_records(t_cb &&cb) { reader.read_records(header, input_range, std::forward <t_cb>(cb)); }
+		void run() override;
+		void streaming_reader_did_parse_header(libbio::bam::in_order_streaming_reader &reader, libbio::bam::header &&hh, libbio::sam::header &&hh_) override;
+		void streaming_reader_did_parse_records(libbio::bam::in_order_streaming_reader &reader, libbio::bam::record_buffer &records) override;
+	};
+}
+
+
+namespace panvc3 {
+	
+	class alignment_input
+	{
+	private:
+		std::unique_ptr <detail::alignment_input>	m_input;
+		
+	private:
+		explicit alignment_input(std::unique_ptr <detail::alignment_input> &&input):
+			m_input(std::move(input))
+		{
+		}
+		
+	public:
+		static alignment_input open_file_handle(
+			libbio::file_handle &&fh,
+			libbio::dispatch::queue &reading_queue, // for BAM
+			libbio::dispatch::serial_queue_base &output_queue,
+			libbio::dispatch::group &group,
+			alignment_input_delegate &delegate,
+			bool input_is_bam
+		);
+		
+		static alignment_input open_path_or_stdin(
+			char const *path,
+			libbio::dispatch::queue &reading_queue, // for BAM
+			libbio::dispatch::serial_queue_base &output_queue,
+			libbio::dispatch::group &group,
+			alignment_input_delegate &delegate
+		);
+			
+		void run() { m_input->run(); }
 	};
 }
 
--- libpanvc3/alignment_input.cc
+++ libpanvc3/alignment_input.cc
@@ -3,20 +3,96 @@
  * This code is licensed under MIT license (see LICENSE for details).
  */
 
+#include <boost/algorithm/string/predicate.hpp>	// boost::iequals
 #include <libbio/file_handling.hh>
 #include <panvc3/alignment_input.hh>
-#include <unistd.h>						// STDIN_FILENO
+#include <unistd.h>								// STDIN_FILENO
 
-namespace lb	= libbio;
+namespace bam		= libbio::bam;
+namespace dispatch	= libbio::dispatch;
+namespace lb		= libbio;
+namespace sam		= libbio::sam;
 
 
 namespace panvc3 {
 	
-	alignment_input alignment_input::open_path_or_stdin(char const *path)
+	alignment_input alignment_input::open_file_handle(
+		libbio::file_handle &&fh,
+		dispatch::queue &reading_queue, // for BAM
+		dispatch::serial_queue_base &output_queue,
+		dispatch::group &group,
+		alignment_input_delegate &delegate,
+		bool input_is_bam
+	)
 	{
-		if (path)
-			return alignment_input(lb::file_handle(lb::open_file_for_reading(path)));
+		if (input_is_bam)
+			return alignment_input{std::make_unique <detail::bam_in_order_alignment_input>(std::move(fh), reading_queue, output_queue, group, delegate)};
 		else
-			return alignment_input(lb::file_handle(STDIN_FILENO, false));
+			return alignment_input{std::make_unique <detail::sam_alignment_input>(std::move(fh), output_queue, group, delegate)};
+	}
+	
+	
+	alignment_input alignment_input::open_path_or_stdin(
+		char const *path,
+		dispatch::queue &reading_queue, // for BAM
+		dispatch::serial_queue_base &output_queue,
+		dispatch::group &group,
+		alignment_input_delegate &delegate
+	)
+	{
+		if (!path)
+			return open_file_handle(lb::file_handle(STDIN_FILENO, false), reading_queue, output_queue, group, delegate, false);
+		
+		// Determine from the file name extension if the input is BAM.
+		lb::file_handle fh(lb::open_file_for_reading(path));
+		auto const path_length(strlen(path));
+		if (4 <= path_length)
+		{
+			std::string_view const path_suffix(path + path_length - 4, 4);
+			if (boost::iequals(path_suffix, std::string_view{".bam"}))
+				return open_file_handle(std::move(fh), reading_queue, output_queue, group, delegate, true);
+		}
+		
+		return open_file_handle(std::move(fh), reading_queue, output_queue, group, delegate, false);
+	}
+}
+
+
+namespace panvc3::detail {
+	
+	void sam_alignment_input::run()
+	{
+		m_output_queue->group_async(*m_group, [this](){
+			m_reader.read_header(m_header, m_input_range);
+			m_delegate->handle_header(m_header);
+			m_reader.read_records(m_header, m_input_range, [this](sam::record &aln_rec){ m_delegate->handle_alignment(aln_rec); });
+		});
+	}
+	
+	
+	void bam_in_order_alignment_input::run()
+	{
+		m_bgzf_reader.run(*m_reading_queue);
+	}
+	
+	
+	void bam_in_order_alignment_input::streaming_reader_did_parse_header(
+		bam::in_order_streaming_reader &reader,
+		bam::header &&hh,
+		sam::header &&hh_
+	)
+	{
+		m_header = std::move(hh_);
+		m_delegate->handle_header(m_header);
+	}
+	
+	
+	void bam_in_order_alignment_input::streaming_reader_did_parse_records(
+		bam::in_order_streaming_reader &reader,
+		bam::record_buffer &records
+	)
+	{
+		for (auto &aln_rec : records)
+			m_delegate->handle_alignment(aln_rec);
 	}
 }
