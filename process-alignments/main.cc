e8a9efd0dda3b9dda87500f1532e8492722ba560
Tuukka Norri <tsnorri@iki.fi>
2024-10-17 14:11:27 +0300

    Add missing headers as reported by clangd

--- process-alignments/main.cc
+++ process-alignments/main.cc
@@ -3,12 +3,24 @@
  * This code is licensed under MIT license (see LICENSE for details).
  */
 
-#include <algorithm>					// std::partition
+#include <algorithm>							// std::partition
+#include <boost/exception/get_error_info.hpp>
+#include <cstddef>
+#include <cstdint>
+#include <cstdlib>
+#include <exception>
 #include <iostream>
+#include <libbio/assert.hh>
 #include <libbio/file_handle.hh>
 #include <libbio/file_handling.hh>
+#include <libbio/generic_parser.hh>
+#include <libbio/utility.hh>
+#include <libbio/sam.hh>
 #include <panvc3/alignment_input.hh>
 #include <panvc3/utility.hh>
+#include <string>
+#include <unistd.h>
+#include <vector>
 #include "cmdline.h"
 
 namespace lb	= libbio;
@@ -26,12 +38,12 @@ namespace {
 	{
 		if (alignments.empty())
 			return;
-		
+
 		// Sort by primary.
 		auto it_(std::partition(alignments.begin(), alignments.end(), [](sam::record const &aln_rec){
 			return aln_rec.is_primary();
 		}));
-		
+
 		// Take the SEQ value from some primary alignment.
 		auto const seq_([&]() -> sam::record::sequence_type const * {
 			for (auto it(alignments.begin()); it != it_; ++it)
@@ -39,16 +51,16 @@ namespace {
 				if (!it->seq.empty())
 					return &(it->seq);
 			}
-			
+
 			return nullptr;
 		}());
-		
+
 		if (!seq_)
 		{
 			std::cerr << "ERROR: No primary alignment found for read ‘" << alignments.front().qname << "’; skipping.\n";
 			return;
 		}
-		
+
 		// Rewrite SEQ if empty.
 		auto const &seq(*seq_);
 		for (; it_ != alignments.end(); ++it_)
@@ -56,7 +68,7 @@ namespace {
 			if (it_->seq.empty())
 				it_->seq = seq;
 		}
-		
+
 		// Output.
 		for (auto const &aln_rec : alignments)
 		{
@@ -64,16 +76,16 @@ namespace {
 			os << '\n';
 		}
 	}
-	
-	
+
+
 	void process_(panvc3::alignment_input &aln_input, std::ostream &os, gengetopt_args_info const &args_info)
 	{
 		os << aln_input.header;
-		
+
 		std::string prev_ref_id;
 		std::vector <sam::record> rec_buffer;
 		std::vector <std::size_t> buffer;
-		
+
 		std::size_t rec_idx{SIZE_MAX};
 		aln_input.read_records(
 			[
@@ -86,29 +98,29 @@ namespace {
 				++rec_idx;
 				if (rec_idx && 0 == rec_idx % 10'000'000)
 					lb::log_time(std::cerr) << "Processed " << rec_idx << " alignments…\n";
-				
+
 				if (rec_buffer.empty())
 				{
 					rec_buffer.emplace_back(aln_rec);
 					return;
 				}
-				
+
 				auto const &eq_class_id(rec_buffer.front().qname);
 				if (aln_rec.qname != eq_class_id)
 				{
 					process_alignment_group(rec_buffer, os, aln_input.header, buffer);
 					rec_buffer.clear();
 				}
-				
+
 				rec_buffer.emplace_back(aln_rec);
 			}
 		);
-		
+
 		if (!rec_buffer.empty())
 			process_alignment_group(rec_buffer, os, aln_input.header, buffer);
 	}
-	
-	
+
+
 	void append_program_info(sam::header &output_header, int const argc, char const * const * const argv)
 	{
 		panvc3::append_sam_program_info(
@@ -120,13 +132,13 @@ namespace {
 			output_header.programs
 		);
 	}
-	
-	
+
+
 	void process(gengetopt_args_info const &args_info, int const argc, char const * const * const argv)
 	{
 		// Open the SAM input and output.
 		auto aln_input(panvc3::alignment_input::open_path_or_stdin(args_info.alignments_arg));
-		
+
 		{
 			auto aln_output_fh{[&] -> lb::file_handle {
 				if (args_info.output_path_arg)
@@ -134,7 +146,7 @@ namespace {
 				else
 					return lb::file_handle(STDOUT_FILENO, false);
 			}()};
-			
+
 			lb::file_ostream os;
 			lb::open_stream_with_file_handle(os, aln_output_fh);
 
@@ -147,11 +159,11 @@ namespace {
 				std::exit(EXIT_FAILURE);
 			}
 #endif
-			
+
 			append_program_info(header, argc, argv); // Adding our program info does not affect parsing.
 			process_(aln_input, os, args_info);
 		}
-		
+
 		lb::log_time(std::cerr) << "Done.\n";
 	}
 }
@@ -162,9 +174,9 @@ int main(int argc, char **argv)
 	gengetopt_args_info args_info;
 	if (0 != cmdline_parser(argc, argv, &args_info))
 		std::exit(EXIT_FAILURE);
-	
+
 	//lb::setup_allocated_memory_logging();
-	
+
 	std::ios_base::sync_with_stdio(false);	// Don't use C style IO after calling cmdline_parser.
 
 	if (args_info.print_invocation_given)
@@ -177,7 +189,7 @@ int main(int argc, char **argv)
 
 	if (args_info.print_pid_given)
 		std::cerr << "PID: " << getpid() << '\n';
-	
+
 	try
 	{
 		process(args_info, argc, argv);
@@ -198,6 +210,6 @@ int main(int argc, char **argv)
 			std::cerr << "Stack trace:\n" << (*trace) << '\n';
 		return EXIT_FAILURE;
 	}
-	
+
 	return EXIT_SUCCESS;
 }
